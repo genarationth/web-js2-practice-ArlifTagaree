8508e1e933690cb4e9e2cfb20f32c77465f9506a
ArlifTagaree <125846400+ArlifTagaree@users.noreply.github.com>
2023-03-11 17:01:03 +0700

    Commit Part2

---
+++ app.js
@@ -0,0 +1,197 @@
+// false
+// true
+// 0
+// 1
+// “0”			
+// “000”			
+// “1”			
+// NaN			
+// Infinity			
+// -Infinity			
+// “”			
+// “20”			
+// “Twenty”			
+// null			
+// undefinited
+
+
+
+//1
+Number(false);
+console.log (Number(false));
+
+String(false);
+console.log (String(false));
+
+Boolean(false);
+console.log (Boolean(false));
+
+
+//2
+
+Number(true);
+console.log (Number(true));
+
+String(true);
+console.log (String(true));
+
+Boolean(true);
+console.log (Boolean(true));
+
+//3
+
+Number(0);
+console.log (Number(0));
+
+String(0);
+console.log (String(0));
+
+Boolean(0);
+console.log (Boolean(0));
+
+//4
+
+Number(1);
+console.log (Number(1));
+
+String(1);
+console.log (String(1));
+
+Boolean(1);
+console.log (Boolean(1));
+
+//5
+
+Number("0");
+console.log (Number("0"));
+
+String("0");
+console.log (String("0"));
+
+Boolean("0");
+console.log (Boolean("0"));
+
+//6
+
+Number("000");
+console.log (Number("000"));
+
+String("000");
+console.log (String("000"));
+
+Boolean("000");
+console.log (Boolean("000"));
+
+//7
+
+Number("1");
+console.log (Number("1"));
+
+String("1");
+console.log (String("1"));
+
+Boolean("1");
+console.log (Boolean("1"));
+
+//8
+
+Number(NaN);
+console.log (Number(NaN));
+
+String(NaN);
+console.log (String(NaN));
+
+Boolean(NaN);
+console.log (Boolean(NaN));
+
+//9
+
+Number(Infinity);
+console.log (Number(Infinity));
+
+String(Infinity);
+console.log (String(Infinity));
+
+Boolean(Infinity);
+console.log (Boolean(Infinity));
+
+//10
+
+Number(-Infinity);
+console.log (Number(-Infinity));
+
+String(-Infinity);
+console.log (String(-Infinity));
+
+Boolean(-Infinity);
+console.log (Boolean(-Infinity));
+
+//11
+
+Number("");
+console.log (Number(""));
+
+String("");
+console.log (String(""));
+
+Boolean("");
+console.log (Boolean(""));
+
+//12
+
+Number("20");
+console.log (Number("20"));
+
+String("20");
+console.log (String("20"));
+
+Boolean("20");
+console.log (Boolean("20"));
+
+//13
+
+Number("Twenty");
+console.log (Number("Twenty"));
+
+String("Twenty");
+console.log (String("Twenty"));
+
+Boolean("Twenty");
+console.log (Boolean("Twenty"));
+
+//14
+
+Number(null);
+console.log (Number(null));
+
+String(null);
+console.log (String(null));
+
+Boolean(null);
+console.log (Boolean(null));
+
+//15
+
+Number(undefined);
+console.log (Number(undefined));
+
+String(undefined);
+console.log (String(undefined));
+
+Boolean(undefined);
+console.log (Boolean(undefined));
+
+// Part2 Challenge 1
+
+let birthYear = 1995;
+let futureYear = 2026;
+console.log(futureYear-birthYear);
+
+// Part2 Challenge 2
+
+let myName = "arlif";
+let address = "nonthaburi";
+let hobby = "football";
+
+
+console.log(`Hi, my name is ${myName} I live in ${address} and enjoy ${hobby} `)
